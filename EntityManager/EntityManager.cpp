62b531d2bfd8a9b973ee3d67707a48c3eaa372e3
Aspasia1337 <thereald3al@proton.me>
2025-01-30 23:50:47 +0100

    Player Classes Defined & Partially Implemented

--- EntityManager/EntityManager.cpp
+++ EntityManager/EntityManager.cpp
@@ -1,6 +1,8 @@
 #include "EntityManager.h"
 
-
+struct vec3 {
+	float x, y, z;
+};
 
 bool EntityManager::populateModule(uintptr_t& module, const char* moduleName) {
 	module = reinterpret_cast<uintptr_t>(GetModuleHandle(moduleName));
@@ -13,43 +15,89 @@ bool EntityManager::populateModule(uintptr_t& module, const char* moduleName) {
 	return true;
 }
 
-bool EntityManager::entityListIterator(std::vector<uintptr_t>& retVector) {
+bool EntityManager::entityListIterator() {
 
 	if (!populateModule(hmoduleClientdll, "client.dll")) {
 		return false;
 	}
 
+	listEntryVector.clear();
+	controllerVector.clear();
+	controllerPawnVector.clear();
+	CCSPlayerControllerVector.clear();
+	C_BaseEntityVector.clear();
+
 	uintptr_t entityList = *(uintptr_t*)(hmoduleClientdll + 0x1A292F0);
 
-	std::vector<uintptr_t> entities;
 	std::vector<uintptr_t> buffer = {};
 
-	for (int i = 0; i < 32; i++) {
-		uintptr_t listEntry = *(uintptr_t*)(entityList + ((8 * (i & 0x7FFF) >> 9) + 16));
+	for (int i = 0; i < 64; i++) {
+		//uintptr_t listEntry = *(uintptr_t*)(entityList + ((8 * (i & 0x7FFF) >> 9) + 16));
+		uintptr_t listEntryTemp = (entityList + ((8 * (i & 0x7FFF) >> 9) + 16));
+		uintptr_t listEntry = *(uintptr_t*)listEntryTemp;
 
-		if (!listEntry)
+		if (!listEntry) {
 			continue;
+		}
+
+		if ((8 * (i & 0x7FFF) >> 9) == 0) {
+			helper->m_Console.printMessage(INFO, "List 1");
+		}
+		else if ((8 * (i & 0x7FFF) >> 9) == 2) {
+			helper->m_Console.printMessage(INFO, "List 2");
+
+		}
+		else if ((8 * (i & 0x7FFF) >> 9) == 2) {
+			helper->m_Console.printMessage(INFO, "List 3");
+
+		}
+
+		//CSSPlayerController
+		uintptr_t entityControllerTemp = ((listEntry + 120 * (i & 0x1FF))); //0x78
+		uintptr_t entityController = *(uintptr_t*)entityControllerTemp;
 
 
-
-		uintptr_t entityController = *(uintptr_t*)((listEntry + 120 * (i & 0x1FF))); //0x78
-
-		if (!entityController)
+		if (!entityController) {
 			continue;
+		}
 
-		uintptr_t entityControllerPawn = *(uintptr_t*)(entityController + 0x80C);
-		if (!entityControllerPawn)
+		//C_CSPlayerPawn
+		uintptr_t entityControllerPawnTemp = (entityController + 0x80C);
+		uintptr_t entityControllerPawn = *(uintptr_t*)entityControllerPawnTemp;
+
+
+
+		if (!entityControllerPawn) {
 			continue;
+		}
 
 
-		//CBasePlayerPawn
-		uintptr_t entity = *(uintptr_t*)(listEntry + 120 * (entityControllerPawn & 0x1FF));
+		//C_CSPlayerPawn
+		uintptr_t entityTemp = (listEntry + 120 * (entityControllerPawn & 0x1FF));
+		uintptr_t entity = *(uintptr_t*)entityTemp;
 
 		if (entity) {
-			buffer.emplace_back(entity);
+			this->listEntryVector.push_back(listEntry);
+			this->controllerVector.push_back(entityController);
+			this->controllerPawnVector.push_back(entityControllerPawn);
+			//this->CCSPlayerControllerVector.push_back((*(CCSPlayerController**)entityControllerTemp));
+			this->C_CSPlayerPawnVector.push_back((*(C_CSPlayerPawn**)entityTemp));
+			//this - C_BaseEntityVector.push_back((*(C_BaseEntity**)entityControllerTemp));
+
+
 		}
 	}
-	helper->m_Console.printMessage(INFO, "Entity Manager found ", buffer.size(), " entities");
-	retVector = buffer;
+
+	helper->m_Console.printMessage(INFO, "Entity Manager found ", this->C_CSPlayerPawnVector.size(), " entities");
+
 	return true;
+}
+
+
+void EntityManager::printPlayerInfo() {
+	vec3* position = nullptr;
+	for (unsigned i = 0; i < entManager->C_CSPlayerPawnVector.size();i++) {
+		helper->m_Console.printMessage(INFO, "Is buyZone? -> ", entManager->C_CSPlayerPawnVector[i]->m_bInBuyZone);
+
+	}
 }
--- EntityManager/EntityManager.h
+++ EntityManager/EntityManager.h
@@ -5,26 +5,66 @@
 #include <vector>
 #include <iostream>
 #include <type_traits>
+
 #include "../helper/helper.h"
 
+#define STR_MERGE_IMPL(a, b) a##b // Combine a & b = ab
+#define STR_MERGE(a, b) STR_MERGE_IMPL(a, b)
+#define MAKE_PAD(size) STR_MERGE(_pad, __COUNTER__)[size]
+#define DEFINE_MEMBER_N(type, name, offset) struct {unsigned char MAKE_PAD(offset); type name;}
 
-class LocalPlayer
-{
-private:
-	uintptr_t DW_Sensitivity;
-public:
-	uintptr_t localPlayerPawn;
-	int Team;
-	int Health;
+class CEntitySystem {
 
-	uint32_t ShotsFired;
-	float AimPunchAngle;
-
-	int m_hPlayerPawn;
-	int EntityIndex;
-	float Sensitivity;
-	int fFlag;
 };
+
+class CGameEntitySystem : CEntitySystem {
+
+};
+
+class CEntityInstance {
+
+};
+
+class C_BaseEntity : CEntityInstance {
+	union {
+		DEFINE_MEMBER_N(int, health, 0x344);
+	};
+};
+
+class CBasePlayerController : C_BaseEntity {
+public:
+
+};
+
+
+class CCSPlayerController : CBasePlayerController {
+public:
+	union {
+		DEFINE_MEMBER_N(char*, playerName, 0x660);
+		DEFINE_MEMBER_N(bool, hasHelmet, 0x821);
+	};
+};
+
+
+class C_BasePlayerPawn : CCSPlayerController {
+
+};
+
+class C_CSPlayerPawnBase : C_BasePlayerPawn {
+
+};
+
+
+class C_CSPlayerPawn : C_CSPlayerPawnBase {
+public:
+	union {
+		DEFINE_MEMBER_N(bool, m_bInBuyZone, 0x1580);
+	};
+
+};
+
+
+
 
 
 class EntityManager
@@ -32,14 +72,24 @@ class EntityManager
 
 private:
 	uintptr_t hmoduleClientdll = 0;
+	std::vector <uintptr_t> listEntryVector;
+	std::vector <uintptr_t> controllerVector;
+	std::vector <uintptr_t> controllerPawnVector;
 	std::vector <uintptr_t> entityVector;
-
+	std::vector <CCSPlayerController*> CCSPlayerControllerVector;
+	std::vector <C_CSPlayerPawn*> C_CSPlayerPawnVector;
+	std::vector <C_BaseEntity*> C_BaseEntityVector;
 
 	bool populateModule(uintptr_t& module, const char* moduleName);
+
+
 public:
 
-	bool entityListIterator(std::vector<uintptr_t>& retVector);
+	bool entityListIterator();
+
+
+	void printPlayerInfo();
+
 };
 
 inline EntityManager* entManager = new EntityManager();
-
