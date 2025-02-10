ea07ec4fe25eeb45e39790e89319eef9b966e83a
Aspasia1337 <thereald3al@proton.me>
2025-02-10 23:32:35 +0100

    Entity Manager Refactor

--- EntityManager/EntityManager.cpp
+++ EntityManager/EntityManager.cpp
@@ -5,32 +5,32 @@
 #include <iostream>
 #include <string>
 
-std::string GameEntitySystem::GetSchemaName(void* entity)
+std::string GameEntitySystem::GetSchemaName (void *entity)
 {
-	const uintptr_t entity_identity = *(uintptr_t*)((uintptr_t)entity + 0x10);
+	const uintptr_t entity_identity = *(uintptr_t *)((uintptr_t)entity + 0x10);
 	if (!entity_identity)return "";
 
-	const uintptr_t entity_class_info = *(uintptr_t*)(entity_identity + 0x8);
+	const uintptr_t entity_class_info = *(uintptr_t *)(entity_identity + 0x8);
 	if (!entity_class_info)return "";
 
-	const uintptr_t schema_class_info_data = *(uintptr_t*)(entity_class_info + 0x30);
+	const uintptr_t schema_class_info_data = *(uintptr_t *)(entity_class_info + 0x30);
 	if (!schema_class_info_data)return "";
 
-	const char* class_name = *(const char**)(schema_class_info_data + 0x8);
+	const char *class_name = *(const char **)(schema_class_info_data + 0x8);
 	if (!class_name) return "";
 
 	char buffer[1024];
 
-	lstrcpyA(buffer, class_name);
-	return std::string(buffer);
+	lstrcpyA (buffer, class_name);
+	return std::string (buffer);
 }
 
-void* GameEntitySystem::GetEntityByIndexFunction(int Index)
+void *GameEntitySystem::GetEntityByIndexFunction (int Index)
 {
 	// sub_606B10 IDA returns the entity
-	using fnGetBaseEntity = uintptr_t * (__thiscall*)(void*, int);
-	static auto GetBaseEntity = reinterpret_cast<fnGetBaseEntity>(helper->m_Mem.PatternScanner("client.dll", "81 FA ? ? ? ? 77 36 8B C2 C1 F8 09 83 F8 3F 77 2C 48 98 48 8B 4C C1 ? 48 85 C9 74 20 8B C2 25 ? ? ? ? 48 6B C0 78 48 03 C8 74 10 8B 41 10 25 ? ? ? ? 3B C2 75 04 48 8B 01 C3"));
-	return GetBaseEntity(*(uintptr_t**)pEntityList, Index);
+	using fnGetBaseEntity = uintptr_t * (__thiscall *)(void *, int);
+	static auto GetBaseEntity = reinterpret_cast<fnGetBaseEntity>(helper->m_Mem.PatternScanner ("client.dll", "81 FA ? ? ? ? 77 36 8B C2 C1 F8 09 83 F8 3F 77 2C 48 98 48 8B 4C C1 ? 48 85 C9 74 20 8B C2 25 ? ? ? ? 48 6B C0 78 48 03 C8 74 10 8B 41 10 25 ? ? ? ? 3B C2 75 04 48 8B 01 C3"));
+	return GetBaseEntity (*(uintptr_t **)pEntityList, Index);
 }
 
 
@@ -40,8 +40,7 @@ void GameEntitySystem::getGameEntities ( ) {
 
 	ControllerVector.clear ( );
 	PawnVector.clear ( );
-
-	//helper->m_Console.printMessage(WARNING,"-----------------------------------");
+	SmokeGrenadeVector.clear ( );
 
 	for (unsigned int i = 0; i < pMaxIndex; i++) {
 
@@ -49,7 +48,6 @@ void GameEntitySystem::getGameEntities ( ) {
 		if (!Entity)
 			continue;
 
-		// helper->m_Console.printMessage(DEBUG, "Found! - ", GetSchemaName(Entity));
 
 		if (GetSchemaName (Entity) == ("C_CSPlayerPawnBase")) {
 
@@ -59,7 +57,6 @@ void GameEntitySystem::getGameEntities ( ) {
 				PawnVector.push_back (Pawn);
 				continue;
 			}
-
 		}
 
 		if (GetSchemaName (Entity) == ("CBasePlayerController")) {
@@ -72,14 +69,25 @@ void GameEntitySystem::getGameEntities ( ) {
 				continue;
 			}
 		}
+
+
+		//Schema
+
+		if (GetSchemaName (Entity) == ("C_BaseCSGrenadeProjectile")) {
+			C_SmokeGrenadeProjectile *SmokeProjectile = (C_SmokeGrenadeProjectile *)Entity;
+			SmokeGrenadeVector.push_back (SmokeProjectile);
+
+		}
 	}
 
 }
 
-void GameEntitySystem::getAllPlayers(){
+void GameEntitySystem::getAllPlayers ( ) {
 
 	getGameEntities ( );
 	PlayersVector.clear ( );
+
+	// Change this to qsort 
 
 	for (unsigned int i = 0; i < PawnVector.size ( ); i++) {
 		for (unsigned int j = 0; j < ControllerVector.size ( ); j++) {
@@ -89,57 +97,21 @@ void GameEntitySystem::getAllPlayers(){
 			}
 		}
 	}
+
 }
 
 
-void GameEntitySystem::glowPatch()
-{
 
-	C_PlayerPawn* aspasiaUser = (C_PlayerPawn*)(clientDll + 0x1889F30);
 
-	Color glowColor = { 0.2f, 1.0f, 0.2f, 1.0f };
 
-	for (unsigned i = 0; i < PawnVector.size();i++) {
 
-		if ((char*)PawnVector[i] == *(char**)aspasiaUser)
-			continue;
+void GameEntitySystem::noFlash ( ) {
 
-		DWORD colorArgb = ((DWORD)(glowColor.w * 255) << 24) |
-			((DWORD)(glowColor.z * 255) << 16) |
-			((DWORD)(glowColor.y * 255) << 8) |
-			((DWORD)(glowColor.x * 255));
+	float *flashtime = (float *)0x00000290467B1478;
+	float *flahscreenshot = (float *)0x00000290467B147C;
 
-		*(DWORD*)((char*)PawnVector[i] + 0xC00 + 0x40) = colorArgb;  // It's mandatory to use char* because void* doesn't allow poitner arithmetic
-		*(DWORD*)((char*)PawnVector[i] + 0xC00 + 0x51) = 1;
-	}
+	*(float *)flashtime = 0;
+	*(float *)flahscreenshot = 0;
 }
 
 
-void GameEntitySystem::renderDot() {
-	float(*ViewMatrix)[4][4] = (float(*)[4][4])(clientDll + 0x1AA17C0);
-
-	for (unsigned i = 0; i < PlayersVector.size();i++) {
-		Vec3 feetPosition = (PlayersVector[i]->Pawn->vOldOrigin);
-
-		Vec3 headPos = { feetPosition.x + 0.0f,feetPosition.y + 0.0f,feetPosition.z + 65.0f };
-
-		Vec2 feet, head;
-
-		if (feetPosition.WorldToScreen(feet, ViewMatrix) && headPos.WorldToScreen(head, ViewMatrix)) {
-			float height = (feet.y - head.y) * 1.5f;
-			float width = height / 1.5f;
-
-			//char playername[128] = {};
-
-			//std::memcpy(playername, PlayersVector[i]->Controller->m_iszPlayerName, sizeof(playername));
-
-
-
-			//ImGui::GetBackgroundDrawList()->AddRect({ (feet.x-width/2),head.y }, {(feet.x-width/2) + width,head.y+height}, ImColor(255,255,255));
-			ImGui::GetBackgroundDrawList ( )->AddText ({ feet.x, feet.y }, ImColor (0, 150, 0), std::to_string (PlayersVector[i]->Pawn->pawnHealth).c_str ( ));
-			ImGui::GetBackgroundDrawList()->AddText({ feet.x, feet.y-30 }, ImColor(0, 0, 80), std::to_string(PlayersVector[i]->Pawn->m_armor).c_str());
-			//ImGui::GetBackgroundDrawList()->AddText({ feet.x, feet.y }, ImColor(0, 255, 0), playername);
-		}
-		
-	}
-}
--- EntityManager/EntityManager.h
+++ EntityManager/EntityManager.h
@@ -15,101 +15,104 @@
 #define DEFINE_MEMBER_N(type, name, offset) struct {unsigned char MAKE_PAD(offset); type name;}
 
 struct vec3 {
-    float x, y, z;
+	float x, y, z;
 };
 
 struct Color {
-    float x; //  (B)
-    float y; //  (G)
-    float z; //  (R)
-    float w; //  (A)
+	float x; //  (B)
+	float y; //  (G)
+	float z; //  (R)
+	float w; //  (A)
 };
-
-
-
 
 class C_PlayerController {
 public:
-    union {
-        //              Type     Name    Offset
-        DEFINE_MEMBER_N(uint32_t, m_hPawn, 0x62c);
-        DEFINE_MEMBER_N (char **, m_iszPlayerName, 0x660);
-        DEFINE_MEMBER_N(bool, pawnIsAlive, 0x814);
-        DEFINE_MEMBER_N(uint32_t, pawnHealth, 0x818);
-        DEFINE_MEMBER_N(uint32_t, m_hOriginalControllerOfCurrentPawn, 0x830); //linker!
+	union {
+		//              Type     Name    Offset
+		DEFINE_MEMBER_N (uint32_t, m_hPawn, 0x62c);
+		DEFINE_MEMBER_N (char, m_iszPlayerName, 0x660);
+		DEFINE_MEMBER_N (bool, pawnIsAlive, 0x814);
+		DEFINE_MEMBER_N (uint32_t, pawnHealth, 0x818);
+		DEFINE_MEMBER_N (uint32_t, m_hOriginalControllerOfCurrentPawn, 0x830); //linker!
 
-        
-    };
+
+	};
 };
 
+class C_SmokeGrenadeProjectile {
+public:
+	union {
+		DEFINE_MEMBER_N (bool, bDidSmokeEffect, 0x1214);
+		DEFINE_MEMBER_N (Vec3, smokeColor, 0x121C);
+
+	};
+};
 
 class C_PlayerPawn {
 public:
-    union {
-        //              Type     Name    Offset
-        DEFINE_MEMBER_N (uint32_t, m_armor, 0x241C);   //linker!
-        DEFINE_MEMBER_N(int32_t, pawnHealth, 0x344);
-        DEFINE_MEMBER_N(uint8_t, isAlive, 0x348);
-        DEFINE_MEMBER_N(Vec3, vOldOrigin, 0x1324);
-        DEFINE_MEMBER_N(char*, playerName, 0x660);
-        DEFINE_MEMBER_N(uint32_t, m_hOriginalController, 0x1508);   //linker!
+	union {
+		//              Type     Name    Offset
+		DEFINE_MEMBER_N (uint32_t, m_armor, 0x241C);   //linker!
+		DEFINE_MEMBER_N (int32_t, pawnHealth, 0x344);
+		DEFINE_MEMBER_N (uint8_t, isAlive, 0x348);
+		DEFINE_MEMBER_N (Vec3, vOldOrigin, 0x1324);
+		DEFINE_MEMBER_N (char *, playerName, 0x660);
+		DEFINE_MEMBER_N (uint32_t, m_hOriginalController, 0x1508);   //linker!
 
-        
-    };
+
+	};
 };
 
 class Players {
 public:
-    C_PlayerPawn* Pawn;
-    C_PlayerController* Controller;
+	C_PlayerPawn *Pawn;
+	C_PlayerController *Controller;
 
-    Players() : Pawn(nullptr), Controller(nullptr) {}
-    Players(C_PlayerPawn* pawn, C_PlayerController* controller)
-        : Pawn(pawn), Controller(controller) {
-    }
+	Players ( ) : Pawn (nullptr), Controller (nullptr) {}
+	Players (C_PlayerPawn *pawn, C_PlayerController *controller)
+		: Pawn (pawn), Controller (controller) {
+	}
 };
 
 class GameEntitySystem {
 
-private:
-    uintptr_t pEntityList;
-    uintptr_t clientDll;
-    DWORD pMaxIndex;
-    std::vector<C_PlayerController*> ControllerVector;
-    std::vector<C_PlayerPawn*> PawnVector;
-    std::vector<Players*> PlayersVector;
-
-    int indexes = 0;
-
 public:
-    
-    GameEntitySystem() {
-        init();
-    }              
+	uintptr_t pEntityList;
+	uintptr_t clientDll;
+	DWORD pMaxIndex;
+	std::vector<C_PlayerController *> ControllerVector;
+	std::vector<C_PlayerPawn *> PawnVector;
+	std::vector<Players *> PlayersVector;
+	std::vector<C_SmokeGrenadeProjectile *> SmokeGrenadeVector;
+	float (*ViewMatrix)[4][4];
 
-    void init() {
-        clientDll = (uintptr_t)GetModuleHandle("client.dll");
-        pEntityList = (uintptr_t)GetModuleHandle("client.dll") + (uintptr_t)0x1B5C6D8;
-        pMaxIndex = *(DWORD*)(*(uintptr_t*)(clientDll + (uintptr_t)0x1A359C0) + (uintptr_t)0x20F0);
-    }
+	int indexes = 0;
 
 
-    std::string GetSchemaName(void* entity);
 
-    void* GetEntityByIndexFunction(int Index);
+	GameEntitySystem ( ) {
+		init ( );
+	}
 
-    void getGameEntities();
+	void init ( ) {
+		clientDll = (uintptr_t)GetModuleHandle ("client.dll");
+		pEntityList = (uintptr_t)GetModuleHandle ("client.dll") + (uintptr_t)0x1B5C6D8;
+		pMaxIndex = *(DWORD *)(*(uintptr_t *)(clientDll + (uintptr_t)0x1A359C0) + (uintptr_t)0x20F0);
+		ViewMatrix = reinterpret_cast<float(*)[4][4]>(helper->m_Mem.ResolveRip (helper->m_Mem.PatternScanner ("client.dll", "48 8D ?? ?? ?? ?? ?? 48 C1 E0 06 48 03 C1 C3 CC CC"), 3, 7));
 
-    void getAllPlayers();
+	}
 
-    void glowPatch();
 
-    void renderDot ( );
+	std::string GetSchemaName (void *entity);
 
-    
+	void *GetEntityByIndexFunction (int Index);
 
-   
+	void getGameEntities ( );
+
+	void getAllPlayers ( );
+
+	void noFlash ( );
 
 };
 
-inline GameEntitySystem* cgame = new GameEntitySystem();
+inline GameEntitySystem *cgame = new GameEntitySystem ( );
