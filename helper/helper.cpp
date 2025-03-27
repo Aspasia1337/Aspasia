34fd9d124040f4a3a8830b2ec6eab797ba027825
Aspasia1337 <thereald3al@proton.me>
2025-03-27 23:11:46 +0100

    Module Helper Added

--- helper/helper.cpp
+++ helper/helper.cpp
@@ -144,3 +144,110 @@ std::uint8_t *Helper::Memory::ResolveRip(std::uint8_t *address, std::uint32_t rv
 
 	return reinterpret_cast<uint8_t *>(rva + rip);
 }
+
+template <typename C> requires (std::is_same_v<C, char> || std::is_same_v<C, wchar_t>)
+constexpr int StringCompareN (const C *tszLeft, const C *tszRight, std::size_t nCount)
+{
+	using ComparisonType_t = std::conditional_t<std::is_same_v<C, char>, std::uint8_t, std::conditional_t<sizeof (wchar_t) == 2U, std::int16_t, std::int32_t>>;
+
+	ComparisonType_t nLeft, nRight;
+	while (nCount--)
+	{
+		nLeft = static_cast<ComparisonType_t>(*tszLeft++);
+		nRight = static_cast<ComparisonType_t>(*tszRight++);
+
+		if (nLeft != nRight)
+			return nLeft - nRight;
+
+		if (nLeft == C ('\0'))
+			break;
+	}
+
+	return 0;
+}
+
+void *GetExportAddress (const void *hModuleBase, const char *szProcedureName)
+{
+	const auto pBaseAddress = static_cast<const std::uint8_t *>(hModuleBase);
+
+	const auto pIDH = static_cast<const IMAGE_DOS_HEADER *>(hModuleBase);
+	if (pIDH->e_magic != IMAGE_DOS_SIGNATURE)
+		return nullptr;
+
+	const auto pINH = reinterpret_cast<const IMAGE_NT_HEADERS64 *>(pBaseAddress + pIDH->e_lfanew);
+	if (pINH->Signature != IMAGE_NT_SIGNATURE)
+		return nullptr;
+
+	const IMAGE_OPTIONAL_HEADER64 *pIOH = &pINH->OptionalHeader;
+	const std::uintptr_t nExportDirectorySize = pIOH->DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size;
+	const std::uintptr_t uExportDirectoryAddress = pIOH->DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+
+	if (nExportDirectorySize == 0U || uExportDirectoryAddress == 0U)
+	{
+		return nullptr;
+	}
+
+	const auto pIED = reinterpret_cast<const IMAGE_EXPORT_DIRECTORY *>(pBaseAddress + uExportDirectoryAddress);
+	const auto pNamesRVA = reinterpret_cast<const std::uint32_t *>(pBaseAddress + pIED->AddressOfNames);
+	const auto pNameOrdinalsRVA = reinterpret_cast<const std::uint16_t *>(pBaseAddress + pIED->AddressOfNameOrdinals);
+	const auto pFunctionsRVA = reinterpret_cast<const std::uint32_t *>(pBaseAddress + pIED->AddressOfFunctions);
+
+	// Perform binary search to find the export by name
+	std::size_t nRight = pIED->NumberOfNames, nLeft = 0U;
+	while (nRight != nLeft)
+	{
+		// Avoid INT_MAX/2 overflow
+		const std::size_t uMiddle = nLeft + ((nRight - nLeft) >> 1U);
+		const int iResult = strcmp (szProcedureName, reinterpret_cast<const char *>(pBaseAddress + pNamesRVA[uMiddle]));
+
+		if (iResult == 0)
+		{
+			const std::uint32_t uFunctionRVA = pFunctionsRVA[pNameOrdinalsRVA[uMiddle]];
+
+			if (uFunctionRVA >= uExportDirectoryAddress && uFunctionRVA - uExportDirectoryAddress < nExportDirectorySize)
+			{
+				break;
+			}
+
+			return const_cast<std::uint8_t *>(pBaseAddress) + uFunctionRVA;
+		}
+
+		if (iResult > 0)
+			nLeft = uMiddle + 1;
+		else
+			nRight = uMiddle;
+	}
+
+	return nullptr;
+}
+
+bool GetSectionInfo (const void *hModuleBase, const char *szSectionName, std::uint8_t **ppSectionStart, std::size_t *pnSectionSize)
+{
+	const auto pBaseAddress = static_cast<const std::uint8_t *>(hModuleBase);
+
+	const auto pIDH = static_cast<const IMAGE_DOS_HEADER *>(hModuleBase);
+	if (pIDH->e_magic != IMAGE_DOS_SIGNATURE)
+		return false;
+
+	const auto pINH = reinterpret_cast<const IMAGE_NT_HEADERS *>(pBaseAddress + pIDH->e_lfanew);
+	if (pINH->Signature != IMAGE_NT_SIGNATURE)
+		return false;
+
+	const IMAGE_SECTION_HEADER *pISH = IMAGE_FIRST_SECTION (pINH);
+
+	for (WORD i = 0U; i < pINH->FileHeader.NumberOfSections; i++, pISH++)
+	{
+		if (StringCompareN (szSectionName, reinterpret_cast<const char *>(pISH->Name), IMAGE_SIZEOF_SHORT_NAME) == 0)
+		{
+			if (ppSectionStart != nullptr)
+				*ppSectionStart = const_cast<std::uint8_t *>(pBaseAddress) + pISH->VirtualAddress;
+
+			if (pnSectionSize != nullptr)
+				*pnSectionSize = pISH->SizeOfRawData;
+
+			return true;
+		}
+	}
+
+	return false;
+}
