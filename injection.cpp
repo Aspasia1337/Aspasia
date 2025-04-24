1b1abd5a3d4555820ffaaa560d4cdc36ab6dafba
Aspasia1337 <thereald3al@proton.me>
2025-04-25 01:43:55 +0200

    Shellcode Fix

--- injection.cpp
+++ injection.cpp
@@ -1,6 +1,82 @@
 #include "injection.h"
 
-void __stdcall ShellCode (MANUAL_MAPPING_DATA *pData);
+#define RELOC_FLAG(RelInfo)((RelInfo >> 0x0C) == IMAGE_REL_BASED_DIR64)
+
+void __stdcall Shellcode (MANUAL_MAPPING_DATA *pData) {
+	if (!pData)
+		return;
+
+	BYTE *pBase = reinterpret_cast<BYTE *>(pData);
+	auto *pOptional = &reinterpret_cast<IMAGE_NT_HEADERS *>(pBase + reinterpret_cast<IMAGE_DOS_HEADER *>(pData)->e_lfanew)->OptionalHeader;
+
+	auto _LoadLibraryA = pData->pLoadLibraryA;
+	auto _GetProcaddress = pData->pGetProcAddress;
+	auto _DLLMain = reinterpret_cast<f_DLL_ENTRY_POINT>(pBase + pOptional->AddressOfEntryPoint);
+
+	BYTE *LocationDelta = pBase - pOptional->ImageBase;
+	
+	if (LocationDelta) {
+		if (!pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].Size) {
+			return; //We cannot relocate the data
+		}
+
+		auto *pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION *>(pBase + pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+		while (pRelocData->VirtualAddress) {
+
+			UINT AmountOfEntries = (pRelocData->SizeOfBlock - sizeof (IMAGE_BASE_RELOCATION)) / sizeof (WORD);
+			WORD *pRelativeInfo = reinterpret_cast<WORD *>(pRelocData + 1);
+			
+			for (UINT i = 0; i != AmountOfEntries; i++, pRelativeInfo++) {
+				if (RELOC_FLAG (*pRelativeInfo)) {
+					UINT_PTR *pPatch = reinterpret_cast<UINT_PTR *>(pBase + pRelocData->VirtualAddress + ((*pRelativeInfo) & 0xFFF));
+					*pPatch += reinterpret_cast<UINT_PTR>(LocationDelta);
+				}
+			}
+
+			pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION *>(reinterpret_cast<BYTE *>(pRelocData) + pRelocData->SizeOfBlock);
+		}
+	}
+
+	if (pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size) {
+		auto *pImportDescriptor = reinterpret_cast<IMAGE_IMPORT_DESCRIPTOR *>(pBase + pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+		while (pImportDescriptor->Name) {
+			char *szMod = reinterpret_cast<char *>(pBase + pImportDescriptor->Name);
+			HINSTANCE hDll = _LoadLibraryA (szMod);
+			ULONG_PTR *pThunkRef = reinterpret_cast<ULONG_PTR *>(pBase + pImportDescriptor->OriginalFirstThunk);
+			ULONG_PTR *pFunctRef = reinterpret_cast<ULONG_PTR *>(pBase + pImportDescriptor->FirstThunk);
+
+			if (!pThunkRef) {
+				pThunkRef = pFunctRef;
+			}
+
+			for (; *pThunkRef; pThunkRef++, pFunctRef++) {
+				if (IMAGE_SNAP_BY_ORDINAL(*pThunkRef)) {
+					*pFunctRef = reinterpret_cast<ULONG_PTR>(_GetProcaddress (hDll, reinterpret_cast<char *>(*pThunkRef & 0xFFFF)));
+				}
+				else {
+					auto *pImport = reinterpret_cast<IMAGE_IMPORT_BY_NAME *>(pBase + (*pThunkRef));
+					*pFunctRef = reinterpret_cast<ULONG_PTR>(_GetProcaddress (hDll, pImport->Name));
+				}
+			}
+			pImportDescriptor++;
+		}
+	}
+
+
+
+	if (pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS].Size) {
+		auto *pTls = reinterpret_cast<IMAGE_TLS_DIRECTORY *>(pBase + pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS].VirtualAddress);
+		auto *pCallback = reinterpret_cast<PIMAGE_TLS_CALLBACK *>(pTls->AddressOfCallBacks);
+		for (; pCallback && *pCallback; pCallback++) {
+			(*pCallback)(pBase, DLL_PROCESS_ATTACH, nullptr);
+		}
+	}
+
+	_DLLMain (pBase, DLL_THREAD_ATTACH, nullptr);
+
+	pData->hMod = reinterpret_cast<HINSTANCE>(pBase);
+	
+}
 
 bool ManualMap (HANDLE hProc, const char *szDllFile) {
 	BYTE *pSrcData = nullptr;
@@ -93,7 +169,7 @@ bool ManualMap (HANDLE hProc, const char *szDllFile) {
 				pSectionHeader->SizeOfRawData, nullptr)) {
 				std::cout << "Cannot map sections\n";
 				delete[] pSrcData;
-				VirtualFreeEx (hProc, pTargetBase, 0, MEM_RELEASE,);
+				VirtualFreeEx (hProc, pTargetBase, 0, MEM_RELEASE);
 				return 0;
 			}
 		}
@@ -137,81 +213,3 @@ bool ManualMap (HANDLE hProc, const char *szDllFile) {
 	return true;
 }
 
-#define RELOC_FLAG(RelInfo)((RelInfo >> 0x0C) == IMAGE_REL_BASED_DIR64)
-
-
-void __stdcall Shellcode (MANUAL_MAPPING_DATA *pData) {
-	if (!pData)
-		return;
-
-	BYTE *pBase = reinterpret_cast<BYTE *>(pData);
-	auto *pOptional = &reinterpret_cast<IMAGE_NT_HEADERS *>(pBase + reinterpret_cast<IMAGE_DOS_HEADER *>(pData)->e_lfanew)->OptionalHeader;
-
-	auto _LoadLibraryA = pData->pLoadLibraryA;
-	auto _GetProcaddress = pData->pGetProcAddress;
-	auto _DLLMain = reinterpret_cast<f_DLL_ENTRY_POINT>(pBase + pOptional->AddressOfEntryPoint);
-
-	BYTE *LocationDelta = pBase - pOptional->ImageBase;
-	
-	if (LocationDelta) {
-		if (!pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].Size) {
-			return; //We cannot relocate the data
-		}
-
-		auto *pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION *>(pBase + pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
-		while (pRelocData->VirtualAddress) {
-
-			UINT AmountOfEntries = (pRelocData->SizeOfBlock - sizeof (IMAGE_BASE_RELOCATION)) / sizeof (WORD);
-			WORD *pRelativeInfo = reinterpret_cast<WORD *>(pRelocData + 1);
-			
-			for (UINT i = 0; i != AmountOfEntries; i++, pRelativeInfo++) {
-				if (RELOC_FLAG (*pRelativeInfo)) {
-					UINT_PTR *pPatch = reinterpret_cast<UINT_PTR *>(pBase + pRelocData->VirtualAddress + ((*pRelativeInfo) & 0xFFF));
-					*pPatch += reinterpret_cast<UINT_PTR>(LocationDelta);
-				}
-			}
-
-			pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION *>(reinterpret_cast<BYTE *>(pRelocData) + pRelocData->SizeOfBlock);
-		}
-	}
-
-	if (pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size) {
-		auto *pImportDescriptor = reinterpret_cast<IMAGE_IMPORT_DESCRIPTOR *>(pBase + pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
-		while (pImportDescriptor->Name) {
-			char *szMod = reinterpret_cast<char *>(pBase + pImportDescriptor->Name);
-			HINSTANCE hDll = _LoadLibraryA (szMod);
-			ULONG_PTR *pThunkRef = reinterpret_cast<ULONG_PTR *>(pBase + pImportDescriptor->OriginalFirstThunk);
-			ULONG_PTR *pFunctRef = reinterpret_cast<ULONG_PTR *>(pBase + pImportDescriptor->FirstThunk);
-
-			if (!pThunkRef) {
-				pThunkRef = pFunctRef;
-			}
-
-			for (; *pThunkRef; pThunkRef++, pFunctRef++) {
-				if (IMAGE_SNAP_BY_ORDINAL(*pThunkRef)) {
-					*pFunctRef = _GetProcaddress (hDll, reinterpret_cast<char *>(*pThunkRef & 0xFFFF));
-				}
-				else {
-					auto *pImport = reinterpret_cast<IMAGE_IMPORT_BY_NAME *>(pBase + (*pThunkRef));
-					*pFunctRef = _GetProcaddress (hDll, pImport->Name);
-				}
-			}
-			pImportDescriptor++;
-		}
-	}
-
-
-
-	if (pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS].Size) {
-		auto *pTls = reinterpret_cast<IMAGE_TLS_DIRECTORY *>(pBase + pOptional->DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS].VirtualAddress);
-		auto *pCallback = reinterpret_cast<PIMAGE_TLS_CALLBACK *>(pTls->AddressOfCallBacks);
-		for (; pCallback && *pCallback; pCallback++) {
-			(*pCallback)(pBase, DLL_PROCESS_ATTACH, nullptr);
-		}
-	}
-
-	_DLLMain (pBase, DLL_THREAD_ATTACH, nullptr);
-
-	pData->hMod = reinterpret_cast<HINSTANCE>(pBase);
-	
-}
